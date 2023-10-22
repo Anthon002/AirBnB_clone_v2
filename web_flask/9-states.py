a936d6293c3f8c015d44688e66e5750ca21dbee8
Anthon002 <chineduanulugwo@gmail.com>
2023-10-22 23:05:18 +0100

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" this will remove sqlalchemy session """
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """
+    Displays all states sorted by name
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """this will render an HTML page with information about the <id>."""
+    for item in storage.all("State").values():
+        if item.id == id:
+            return render_template("9-states.html", item=item)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """this will remove the SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")i
